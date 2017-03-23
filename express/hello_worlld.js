b44b4c52857fa61d2977debc8ff16c541d1e5b31
reubinoff <Moshe.Reubinoff@harmonicinc.com>
2017-03-23 10:33:09 +0200

    ex1

---
+++ express/hello_worlld.js
@@ -0,0 +1,15 @@
+const Express = require('express')
+
+
+
+
+const port = process.argv[2]
+
+var app = Express()
+app.get('/home', function(req, res) {
+    res.end('Hello World!')
+})
+app.listen(port)
+
+
+expressworks
